d46c2be3ae518e10cdaf6a6e9d0309d5923a0c33
森本晃輔 <8tako8tako8@gmail.com>
2023-04-29 20:37:13 +0900

    エラー修正：未定義のiが使用されていた

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ if Rails.env.development?
 end
 
 if Rails.env.production?
-  product = Product.new(name: "商品#{i}", price: 1000)
+  product = Product.new(name: "商品1", price: 1000)
   file_path = Rails.root.join('db/seeds/sample.jpg')
   file = File.open(file_path)
   product.image.attach(io: file, filename: 'sample.jpg', content_type: 'image/jpeg')
