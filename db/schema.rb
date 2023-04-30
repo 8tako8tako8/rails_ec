3d87a3f23a16ad2ce20c5c314a38907f17378b5d
森本晃輔 <8tako8tako8@gmail.com>
2023-04-30 18:13:01 +0900

    productsのnameとpriceにnot nullを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_29_093252) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_30_085155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,8 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_29_093252) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.decimal "price"
+    t.string "name", null: false
+    t.decimal "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
