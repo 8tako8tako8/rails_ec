1d6ee5d0a61d17d0fa68cf71c3b515ebd7371d59
森本晃輔 <8tako8tako8@gmail.com>
2023-05-02 19:12:20 +0900

    RuboCop

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
-  def index
-    
-  end
+  def index; end
 
   def add_to_cart
-    cart_product = current_cart.cart_products.find_by(product_id: params[:product_id])
+    product_id = params[:product_id]
+    quantity = params[:quantity].to_i
+    cart_product = current_cart.cart_products.find_by(product_id: product_id)
     if cart_product
-      cart_product.quantity += params[:quantity].to_i
+      cart_product.quantity += quantity
       cart_product.save!
     else
-      current_cart.cart_products.create!(product_id: params[:product_id], quantity: params[:quantity])
+      current_cart.cart_products.create!(product_id: product_id, quantity: quantity)
     end
     redirect_to carts_path
   end
--- db/migrate/20230430121112_add_foreign_key_to_cart_products.rb
+++ db/migrate/20230430121112_add_foreign_key_to_cart_products.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddForeignKeyToCartProducts < ActiveRecord::Migration[7.0]
   def change
     add_foreign_key :cart_products, :carts
