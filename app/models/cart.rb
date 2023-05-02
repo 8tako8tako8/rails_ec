3e6c56f17147000af31a7bd66fb6beaf45582e8a
森本晃輔 <8tako8tako8@gmail.com>
2023-05-02 18:46:46 +0900

    cartモデルに合計金額と合計数量の計算処理を追加

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,14 @@
 class Cart < ApplicationRecord
   has_many :cart_products, dependent: :destroy
+
+  def total_quantity
+    cart_products.sum(:quantity)
+  end
+
+  def total_price
+    each_prices = cart_products.map do |cart_product|
+      cart_product.product.price * cart_product.quantity
+    end
+    each_prices.sum
+  end
 end
