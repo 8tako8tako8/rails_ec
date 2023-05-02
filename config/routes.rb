e3178d3535c1e04be91871114083c2d40dcc31ef
森本晃輔 <8tako8tako8@gmail.com>
2023-05-02 18:34:33 +0900

    routes：rootにproducts#indexとなるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :products, only: %i[index show]
+  root 'products#index'
+  resources :products, only: %i[show]
   resources :carts, only: %i[index]
   post 'carts/add-to-cart', to: 'carts#add_to_cart'
   delete 'carts/remove-from-cart', to: 'carts#remove_from_cart'
