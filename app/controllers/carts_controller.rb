1909592b3c8e12962da5868dd84493f971f3f40c
森本晃輔 <8tako8tako8@gmail.com>
2023-05-02 18:29:35 +0900

    carts_controllerにadd_to_cartを追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,4 +4,16 @@ class CartsController < ApplicationController
   def index
     
   end
+
+  def add_to_cart
+    cart_product = current_cart.cart_products.find_by(product_id: params[:product_id])
+    if cart_product
+      cart_product.quantity += params[:quantity].to_i
+      cart_product.save!
+    else
+      current_cart.cart_products.create!(product_id: params[:product_id], quantity: params[:quantity])
+    end
+    redirect_to carts_path
+  end
+
 end
