01183f5d96e5171fa743c1b64655e6c67c7256da
森本晃輔 <8tako8tako8@gmail.com>
2023-05-11 02:35:59 +0900

    マージミスを修正

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,14 +1,7 @@
 class OrderMailer < ApplicationMailer
   def order_confirm(order)
     @order = order
-<<<<<<< HEAD
-    @order_details = order_details
-    mail(to: order[:email], subject: '注文完了のお知らせ')
-    end
-=======
     @order_details = order[:order_details]
-    logger.info "++b++"
     mail(to: order[:email], subject: '注文完了のお知らせ')
->>>>>>> de21677de71c1bdb146e0f5f43809c6bbded0b42
   end
 end
