c2b017c0f926fb4ff299f92cbbc5bd91ffca3cae
森本晃輔 <8tako8tako8@gmail.com>
2023-05-11 04:05:37 +0900

    RuboCop

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
-  has_many :order_details
+  has_many :order_details, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,15 +72,9 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => 'apikey',
-    :password => ENV['SENDGRID_API_KEY'],
-    :domain => 'herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.smtp_settings = { user_name: 'apikey', password: ENV['SENDGRID_API_KEY'],
+                                         domain: 'herokuapp.com', address: 'smtp.sendgrid.net',
+                                         port: 587, authentication: :plain, enable_starttls_auto: true }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
