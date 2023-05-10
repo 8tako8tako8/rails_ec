dc07585c5d126036c1fd27ab020d4358a129814c
森本晃輔 <8tako8tako8@gmail.com>
2023-05-11 01:00:08 +0900

    メールの送信先にパラメータを指定する

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,8 @@ class OrderMailer < ApplicationMailer
   def order_confirm(order, order_details)
     @order = order
     @order_details = order_details
-    mail(to: 'example@example.com', subject: '注文完了のお知らせ')
+    if order[:email].blank?
+      mail(to: order[:email], subject: '注文完了のお知らせ')
+    end
   end
 end
