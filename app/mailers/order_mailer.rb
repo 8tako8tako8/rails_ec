110339ecedd8734bd463cd9cad13b1470a0104de
森本晃輔 <8tako8tako8@gmail.com>
2023-05-11 01:52:12 +0900

    fff

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ApplicationMailer
     @order_details = order[:order_details]
     logger.info "++b++"
     if order[:email].present?
-      mail(to: order[:email], subject: '注文完了のお知らせ').deliver_later
+      mail(to: order[:email], subject: '注文完了のお知らせ').deliver_now
       logger.info "++c++"
     end
   end
