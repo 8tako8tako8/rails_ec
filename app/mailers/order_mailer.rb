517165721fe364fdba9c70d722305cde9cc894bf
森本晃輔 <8tako8tako8@gmail.com>
2023-05-11 01:15:11 +0900

    tee

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,7 @@ class OrderMailer < ApplicationMailer
   def order_confirm(order, order_details)
     @order = order
     @order_details = order_details
-    if order[:email].blank?
+    if order[:email].present?
       mail(to: order[:email], subject: '注文完了のお知らせ')
       logger.info "++#{order[:email]}"
     end
