e9b59fb37a9bab9fb87c628095e3df5e43e508a4
森本晃輔 <8tako8tako8@gmail.com>
2023-05-11 01:39:24 +0900

    jj

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ApplicationMailer
     @order_details = order[:order_details]
     logger.info "++before mail"
     if order[:email].present?
-      mail(to: order[:email], subject: '注文完了のお知らせ')
+      mail(to: order[:email], subject: '注文完了のお知らせ').deliver_now
       logger.info "++#{order[:email]}"
     end
   end
