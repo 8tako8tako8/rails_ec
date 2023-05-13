81322a3bb7ca07cff86be287e2e970a3efc4736b
森本晃輔 <8tako8tako8@gmail.com>
2023-05-13 18:22:09 +0900

    注文時にカート商品をリクエストパラメータに含めないように修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,15 +4,15 @@ class OrdersController < ApplicationController
   before_action :basic_auth, only: %i[index show], if: -> { Rails.env.production? }
 
   def create
-    return if order_details_blank?(order_params)
+    return if cart_products_blank?(order_params, current_cart.cart_products)
 
-    request_order = order_params.except(:order_details)
+    order_details = set_order_details
     begin
-      order(order_params)
+      order(order_params, order_details)
     rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved => e
-      set_err_params_and_redirect(e.record.errors.full_messages, request_order)
+      set_err_params_and_redirect(e.record.errors.full_messages, order_params)
     rescue StandardError
-      set_err_params_and_redirect(['予期しないエラー'], request_order)
+      set_err_params_and_redirect(['予期しないエラー'], order_params)
     end
   end
 
@@ -36,22 +36,34 @@ class OrdersController < ApplicationController
     params.require(:order).permit(
       :first_name, :last_name, :email, :prefecture, :address, :address2,
       :card_name, :card_number, :card_expiration_date, :card_cvv,
-      :quantity, :total_price,
-      order_details: %i[product_name product_sku quantity unit_price]
+      :quantity, :total_price
     )
   end
 
-  def order(order_params)
-    ApplicationRecord.transaction { register_order(order_params) }
-    OrderMailer.order_confirm(order_params).deliver_now if order_params[:email].present?
+  def set_order_details
+    order_details = []
+    current_cart.cart_products.each do |cart_product|
+      order_details << {
+        product_sku: cart_product.product.sku,
+        product_name: cart_product.product.name,
+        unit_price: cart_product.product.price,
+        quantity: cart_product.quantity
+      }
+    end
+    order_details
+  end
+
+  def order(request_order, request_order_details)
+    ApplicationRecord.transaction { register_order(request_order, request_order_details) }
+    OrderMailer.order_confirm(request_order, request_order_details).deliver_now
     flash[:notice] = '購入ありがとうございます'
     redirect_to root_path
   end
 
-  def register_order(request_order)
+  def register_order(request_order, request_order_details)
     order = create_order(request_order)
-    request_order[:order_details].each do |order_detail|
-      create_order_detail(order, order_detail)
+    request_order_details.each do |order_detail|
+      create_order_detail(order, order_detail[:product_sku], order_detail[:product_name], order_detail[:unit_price], order_detail[:quantity])
     end
     current_cart.destroy!
   end
@@ -67,18 +79,18 @@ class OrdersController < ApplicationController
     )
   end
 
-  def create_order_detail(order, order_detail)
+  def create_order_detail(order, product_sku, product_name, unit_price, quantity)
     order.order_details.create!(
-      product_sku: order_detail[:product_sku],
-      product_name: order_detail[:product_name],
-      unit_price: order_detail[:unit_price],
-      quantity: order_detail[:quantity]
+      product_sku: product_sku,
+      product_name: product_name,
+      unit_price: unit_price,
+      quantity: quantity
     )
   end
 
-  def order_details_blank?(order_params)
-    if order_params[:order_details].blank?
-      @order = Order.new(order_params)
+  def cart_products_blank?(request_order, cart_products)
+    if cart_products.blank?
+      @order = Order.new(request_order)
       flash.now[:error_messages] = ['カートが空です']
       render 'carts/index', status: :unprocessable_entity
       return true
