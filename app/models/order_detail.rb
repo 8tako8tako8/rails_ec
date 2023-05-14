8c21e28eb1da64ef3de5d7266a88a35b65e0ec23
森本晃輔 <8tako8tako8@gmail.com>
2023-05-14 23:28:16 +0900

    mapを使うようにリファクタリング

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -9,15 +9,13 @@ class OrderDetail < ApplicationRecord
   validates :quantity, presence: true
 
   def self.create_order_details(cart_products)
-    order_details = []
-    cart_products.each do |cart_product|
-      order_details << OrderDetail.new(
+    cart_products.map do |cart_product|
+      OrderDetail.new(
         product_sku: cart_product.product.sku,
         product_name: cart_product.product.name,
         unit_price: cart_product.product.price,
         quantity: cart_product.quantity
       )
     end
-    order_details
   end
 end
