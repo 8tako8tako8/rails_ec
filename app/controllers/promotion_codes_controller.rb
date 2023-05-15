49a22a24acd63daf4361703c5ae9aca5c5ef4bdb
森本晃輔 <8tako8tako8@gmail.com>
2023-05-16 01:56:01 +0900

    PromotionCodesControllerのリファクタリング

--- app/controllers/promotion_codes_controller.rb
+++ app/controllers/promotion_codes_controller.rb
@@ -2,22 +2,35 @@
 
 class PromotionCodesController < ApplicationController
   def use
-    ApplicationRecord.transaction do
-      promotion_code = PromotionCode.find_by(code: params[:code])
-      if !current_cart.promotion_code.nil?
-        flash[:alert] = 'プロモーションコードを利用できるのは１回限りです'
-      elsif promotion_code.nil?
-        flash[:alert] = '無効なプロモーションコードです'
-      elsif promotion_code.is_used?
-        flash[:alert] = '既に使用済みのプロモーションコードです'
-      elsif current_cart.cart_products.empty?
-        flash[:alert] = 'カートが空です'
-      else
-        current_cart.update!(promotion_code: promotion_code)
-        promotion_code.update!(is_used: true)
-        flash[:notice] = 'プロモーションコードが適用されました'
-      end
+    promotion_code = PromotionCode.find_by(code: params[:code])
+    return err_params_and_render(['無効なプロモーションコードです']) if promotion_code.nil?
+    return err_params_and_render(['プロモーションコードを利用できるのは１回限りです']) unless current_cart.promotion_code.nil?
+    return err_params_and_render(['既に使用済みのプロモーションコードです']) if promotion_code.is_used?
+    return err_params_and_render(['カートが空です']) if current_cart.cart_products.empty?
+
+    begin
+      apply_promotion_code(promotion_code)
+    rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved => e
+      err_params_and_render(e.record.errors.full_messages)
+    rescue StandardError
+      err_params_and_render(['予期しないエラー'])
     end
-    redirect_to carts_path
+  end
+
+  private
+
+  def apply_promotion_code(promotion_code)
+    ApplicationRecord.transaction do
+      current_cart.update!(promotion_code: promotion_code)
+      promotion_code.update!(is_used: true)
+    end
+    flash.now[:notice] = 'プロモーションコードが適用されました'
+    render 'carts/index', status: :unprocessable_entity
+  end
+
+  def err_params_and_render(messages)
+    @order = Order.new
+    flash.now[:error_messages] = messages
+    render 'carts/index', status: :unprocessable_entity
   end
 end
